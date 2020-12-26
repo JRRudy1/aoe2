e1bf4199da0311783dde482575b41cd765bd9905
JRRudy1 <31031841+JRRudy1@users.noreply.github.com>
2020-12-26 15:26:40 -0600

    Changes

--- aoe2plot.py
+++ aoe2plot.py
@@ -1,6 +1,6 @@
 
 from aoe2tools import BuildOrderCollection
-from aoe2tools.builds import *
+from builds import *
 
 if __name__ == '__main__':
     v = 0
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     rush_builds = BuildOrderCollection(pop21_scouts,pop22_MaA_archers, pop23_archers)
     other_builds = BuildOrderCollection(pop22_scouts2unique,pop28_turk_fi)
 
-    FC_builds.plot("C:\\Users\\Joshua\\PycharmProjects\\aoe2\\fc.png", v=v)
-    rush_builds.plot("C:\\Users\\Joshua\\PycharmProjects\\aoe2\\rush.png", v=v)
-    other_builds.plot("C:\\Users\\Joshua\\PycharmProjects\\aoe2\\other.png", v=v)
+    FC_builds.plot(".\\fc_builds.png", v=v)
+    rush_builds.plot(".\\rush_builds.png", v=v)
+    other_builds.plot(".\\other_builds.png", v=v)
 
     print('Done.')
